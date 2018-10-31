990315b163b9c77e3b1229fd20a2e61b8f3a4417
Spring <spwu@creams.io>
2018-10-31 19:09:54 +0800

    update

--- src/test/java/io/wooo/security/SecurityApplicationTests.java
+++ src/test/java/io/wooo/security/SecurityApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SecurityDemoApplicationTests {
+public class SecurityApplicationTests {
 
 	@Test
 	public void contextLoads() {
